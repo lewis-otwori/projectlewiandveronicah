7806df9b24a272cf71c4fcc13ac596306f2afea2
veronicahmbuvi <mbuviveronicah3@gmail.com>
2023-05-03 23:00:17 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ export const ToggleContainer = React.createContext()
 
 function App() {
 
-    const [mode, setMode]=useState('light')
+    const [mode, setMode] = useState('light')
 
-    const toggleMode = ()=> {
-        setMode (mode === "light"  ? "dark" : "light")
+    const toggleMode = () => {
+        setMode(mode === "light" ? "dark" : "light")
     }
     const [themeSwitch, setThemeSwitch] = useState(false);
-
+    const [search, setSearch] = useState("")
     const [data, setData] = useState([{}])
 
     useEffect(() => {
@@ -25,20 +25,33 @@ function App() {
             .then(res => res.json())
             .then(data => setData(data))
     }, [])
-
+    function handleSearch() {
+        fetch(` http://localhost:3000/games?title_like=${search}`)
+            .then(res => res.json())
+            .then(data => setData(data));
+    }
     return (
         <ToggleContainer.Provider value={{ themeSwitch, setThemeSwitch }}>
 
             <div className={`App ${mode}`}>
                 <NavBar />
-                <SearchBar/>
-                <button onClick={toggleMode} style={{position: "absolute,", right: 0}}>{mode === "light" ? "Darkmode" : "Lightmode"}</button>
-                
+                <div>
+                    <input
+                        className="srch"
+                        type="text"
+                        placeholder="Search title"
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                    <button onClick={handleSearch}>Search</button>
+                </div>
+                <button onClick={toggleMode} style={{ position: "absolute,", right: 0 }}>{mode === "light" ? "Darkmode" : "Lightmode"}</button>
+
                 <div className='PlayGames'>
                     <img src={themeSwitch ? Letsplay : Game} alt='' id={themeSwitch ? { Letsplay } : { Game }} />
                 </div>
                 <Gamelist data={data} />
-                <AddGame/>
+                <AddGame />
                 <Main /></div>
         </ToggleContainer.Provider>
     )
